9f8fe7a8011c57e6f96ef975745ed0a1a81b954e
diglopes <diego-chez@hotmail.com>
2019-07-30 07:10:48 -0300

    feat: creates and set styles to header

---
+++ src/views/Contato.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Contato</div>
+</template>
+
+<script>
+export default {
+  name: "Contato"
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Cursos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Cursos</div>
+</template>
+
+<script>
+export default {
+  name: "Cursos"
+};
+</script>
+
+<style>
+</style>
